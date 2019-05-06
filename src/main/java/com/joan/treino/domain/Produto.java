9937797df0d4ea0ca1a8f889ef15938188e3b3a7
Joan Fortes <joanfortes@hotmail.com>
2019-05-05 22:35:38 -0300

    Ajustes no endpoint / categorias/{id}

--- src/main/java/com/joan/treino/domain/Produto.java
+++ src/main/java/com/joan/treino/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -21,6 +23,7 @@ public class Produto implements Serializable {
 	private Integer id;
 	private String nome;
 	private double preco;
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CAATEGORIA",
 		joinColumns = @JoinColumn(name = "produto_id"),
--- src/main/java/com/joan/treino/services/CategoriaService.java
+++ src/main/java/com/joan/treino/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.joan.treino.domain.Categoria;
 import com.joan.treino.repositories.CategoriaRepository;
+import com.joan.treino.services.execptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
