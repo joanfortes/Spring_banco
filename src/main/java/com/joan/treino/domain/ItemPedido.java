47cfdf3723133fa2f45541da4504137576f0f514
Joan Fortes <joanfortes@hotmail.com>
2019-05-19 16:53:29 -0300

    inserindo pedido

--- src/main/java/com/joan/treino/domain/ItemPedido.java
+++ src/main/java/com/joan/treino/domain/ItemPedido.java
@@ -59,9 +59,19 @@ public class ItemPedido implements Serializable {
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
 	public Produto getProduto() {
 		return id.getProduto();
 	}
+	
+	public void setProduto (Produto produto) {
+		id.setProduto(produto);
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/com/joan/treino/services/PedidoService.java
+++ src/main/java/com/joan/treino/services/PedidoService.java
@@ -1,13 +1,22 @@
 package com.joan.treino.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.joan.treino.domain.ItemPedido;
+import com.joan.treino.domain.PagamentoComBoleto;
 import com.joan.treino.domain.Pedido;
+import com.joan.treino.domain.enums.EstadoPagamento;
+import com.joan.treino.repositories.ItemPedidoRepository;
+import com.joan.treino.repositories.PagamentoRepository;
 import com.joan.treino.repositories.PedidoRepository;
 import com.joan.treino.services.execptions.ObjectNotFoundException;
+
+
 
 @Service
 public class PedidoService {
@@ -15,10 +24,41 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
-	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreço(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
+	}
 }
